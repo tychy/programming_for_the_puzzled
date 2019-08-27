1be3702b04f48e52ba0c7c9ee5a9c0d8a28d0152
tychy <a2sin2a2ko@gmail.com>
2019-08-27 16:00:04 +0900

    update : add 'H' pass

--- chap_1/caps_onepass.py
+++ chap_1/caps_onepass.py
@@ -5,21 +5,33 @@ cap2 = ['F', 'F', 'B', 'B', 'B', 'F',
 
 cap3 = ['F', 'B', 'F']
 cap4 = []
+cap5 = ['F', 'F', 'B', 'H', 'B', 'B', 'F', 
+'B', 'B', 'B', 'F', 'F', 'F', 'F']
+
  # F B F B
 def pleaseConform(caps):
     if caps == []:
         print('caps is []')
         return
     caps = caps + [caps[0]]
+    start = 0
     for i in range(1, len(caps)):
+        if caps[i] == 'H':
+            if caps[i - 1] != caps[0]:
+                if start == i - 1:
+                    print('flip your cap!')
+                else:
+                    print('through ', i - 1, ',please flip your caps')
+            continue
         if caps[i] != caps[i - 1]:
             if caps[i] != caps[0]:
                 print('People in positions',i,end=' ')
+                start = i
             else:
-                if caps[i - 1] != caps[i - 2]:
+                if start == i - 1:
                     print('flip your cap!')
                 else:
                     print('through ', i - 1, ',please flip your caps')
 
 
-pleaseConform(cap4)
+pleaseConform(cap3)
