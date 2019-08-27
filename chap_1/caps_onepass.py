c47fc23e6293a5e22781a7b06db1f43d613cc46b
tychy <a2sin2a2ko@gmail.com>
2019-08-27 15:37:24 +0900

    add : prob 1

--- chap_1/caps_onepass.py
+++ chap_1/caps_onepass.py
@@ -3,15 +3,19 @@ cap1 = ['F', 'F', 'B', 'B', 'B', 'F',
 cap2 = ['F', 'F', 'B', 'B', 'B', 'F', 
 'B', 'B', 'B', 'F', 'F', 'F', 'F']
 
-
+cap3 = ['F', 'B', 'F']
+ # F B F B
 def pleaseConform(caps):
     caps = caps + [caps[0]]
     for i in range(1, len(caps)):
         if caps[i] != caps[i - 1]:
             if caps[i] != caps[0]:
-                print("People in positions" + str(i))
+                print('People in positions',i,end=' ')
             else:
-                print("through " + str(i - 1) + ",please flip your caps")
+                if caps[i - 1] != caps[i - 2]:
+                    print('flip your cap!')
+                else:
+                    print('through ', i - 1, ',please flip your caps')
 
 
-pleaseConform(cap2)
+pleaseConform(cap3)
